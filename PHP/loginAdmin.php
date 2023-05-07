ff2c6fd62af4f8c2079d0f6c7922005ae717c93e
Ivan Lap <ielizalde184@gmail.com>
2023-05-07 11:51:16 -0600

    Agregado funciones de admin

--- PHP/loginAdmin.php
+++ PHP/loginAdmin.php
@@ -4,11 +4,11 @@
     $usuario = $_POST['nombre'];
     $contra = $_POST['contrasena'];
 
-    $sql = mysqli_query($con, "SELECT nombre, contrasena FROM admins WHERE nombre = '$usuario' AND contrasena = '$contra'");
+    $sql = mysqli_query($con, "SELECT nombre, contrasena FROM admin WHERE nombre = '$usuario' AND contrasena = '$contra'");
     
 
     if($sql -> num_rows > 0){
-        header("location: ../BD/InicioAdmin.php");
+        header("location: ../html/prueba.html");
     }else{
         echo "no";
     }
