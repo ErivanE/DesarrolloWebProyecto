08eee34ae50618b8d0fc6ca2fdd033b3d114e7f4
ErivanE <ielizalde185@gmail.com>
2023-07-11 10:24:07 -0600

    prueba virtualizacion

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,8 +1,8 @@
 <?php
     $server = "localhost";
     $database = "kspgames";
-    $username = "root";
-    $password = "";
+    $username = "ivan";
+    $password = "12345";
 
     $con = mysqli_connect($server, $username, $password, $database);
 ?>
