3cf7306c4a2ed6e2d234cb6faf0459502fed6681
ErivanE <ielizalde185@gmail.com>
2023-07-12 14:39:10 -0600

    a

--- index.php
+++ index.php
@@ -50,23 +50,8 @@
             <div>
                 <div class="agregados-container">
                     <?php
-                        $query ="SELECT * FROM productos ORDER BY id DESC LIMIT 6";
-                        #$result = mysqli_query($con, $query);
-                        $result = $con->query($query);
-                        echo "<h2>Banderin</h2>";
-                        if($result = $result->num_rows>0){
-                            while($row = $result ->fetch_assoc()){
-                                $nombre = $row['titulo'];
-                                $url_img = $row['url_img'];
-                                //TARJETA
-                                echo '<div class="agregados-tarjeta">';
-                                echo    '<a href="#">';
-                                echo        '<img src= "img/productos/'.$url_img.'" width="200" height="300" alt="producto">';
-                                echo    '</a>';
-                                echo    '<p>'.$nombre.'</p>';
-                                echo '</div>';
-                            }
-                        }
+                        // $query ="SELECT * FROM productos ORDER BY id DESC LIMIT 6";
+                        // $result = mysqli_query($con, $query);
                         // while($row = mysqli_fetch_assoc($result)){
                         //     $nombre = $row['titulo'];
                         //     $url_img = $row['url_img'];
