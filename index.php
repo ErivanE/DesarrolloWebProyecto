6ce0fbe91081b566e1ca261f9d55b868d144e322
ErivanE <ielizalde185@gmail.com>
2023-10-21 10:32:22 -0600

    prueba index

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include 'PHP/conexion.php';
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,14 +10,10 @@ include 'PHP/conexion.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
+    
     <link rel="stylesheet" href="VistaUsuario/css/bootstrap.min.css">
     <link rel="stylesheet" href="VistaUsuario/css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-        integrity="sha512-KXmDlF1ri1m3+oQTxo2dPXHoqr/qndCS0fVwX6rD38eq8yzG0BZNA26DrpYToPENdzmsS2IAGU8AMr8NZlTjMQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 
 <body>
